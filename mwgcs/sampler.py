ac9401827388e7b93a2bff7a836e430b392f1fa5
Jay <jaymariebaptista@gmail.com>
2025-09-23 18:25:23 -0700

    feat: add GCS_MASS_HARRIS function for halo mass–GCS mass relation

--- mwgcs/sampler.py
+++ mwgcs/sampler.py
@@ -34,6 +34,14 @@ def GCS_MASS_LINEAR(stellar_mass, g0=-0.725, g1=0.788):
     """
     return 10 ** (g0 + g1 * np.log10(stellar_mass))
 
+def GCS_MASS_HARRIS(halo_mass, g0=-0.725, g1=0.788):
+    """
+    Implementation of the Harris halo mass–GCS mass relation from
+    Harris, Blakeslee, & Harris (2017) paper.
+    """
+    eta = 2.9e-5
+    return eta * halo_mass
+
 
 def GCS_MASS_EADIE(stellar_mass, b0=-10.83, b1=1.59, g0=-0.83, g1=0.8):
     """
